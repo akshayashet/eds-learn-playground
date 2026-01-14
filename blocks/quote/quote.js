b34073fd09967afc29289c786acd0ba5a5025a10
Akshaya Shet <akshayashet@gmail.com>
2026-01-14 19:28:36 +0530

    Add options quotes

---
+++ blocks/quote/quote.js
@@ -0,0 +1,8 @@
+export default function decorate(block) {
+  const [quoteWrapper] = block.children;
+
+  const blockQuote = document.createElement('blockquote');
+  blockQuote.className = 'quote-blockquote';
+  blockQuote.textContent = quoteWrapper.textContent.trim();
+  quoteWrapper.replaceChildren(blockQuote);
+}
